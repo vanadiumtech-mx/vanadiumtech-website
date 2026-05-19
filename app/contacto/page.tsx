49e24795b6a9971c3b005f4cf9a73ca5bd61a4fb
Miguel Baruck Bello Ramirez <miguelbaruckbelloramirez@MacBook-de-Baruck.local>
2026-05-18 22:58:13 -0600

    cambio de direccion en el footer y en el contacto

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -112,17 +112,7 @@ export default function ContactoPage() {
                   ))}
                 </div>
 
-                <div className={styles.map}>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.552153870283!2d-99.162974!3d19.428858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff1c5e9b6e2b%3A0x8e0c8b2e5e8c0b2e!2sPaseo%20de%20la%20Reforma%2C%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1700000000000!5m2!1ses!2smx"
-                    width="100%"
-                    height="250"
-                    style={{ border: 0 }}
-                    allowFullScreen
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe>
-                </div>
+       
               </motion.div>
             </div>
           </div>
